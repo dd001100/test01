fab9ca50c1abc57c3dcf34af91425cab97b53f2a
g123456789123456 <1975214303@qq.com>
2021-06-21 15:56:47 +0800

    "九九乘法表"

---
+++ new02_fd.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+""" 
+@Time    : 2021/6/21 15:28
+@Author  : gan
+"""
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(j ,"*" ,i ,"=",i * j,end="\t")
+    print(" ")
+
+# i = 1
+
+# while i < 10:
+#     l = 1
+#     while l <= i :
+#         print(l,"*" ,i ,"=" ,l*i,end="\t")
+#         l += 1
+#     i += 1
+#     print(" ")
+
+
+
+
